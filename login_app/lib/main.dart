fcea157b30671eb9c080d3928c45fc5afc2920ce
robinNcode <msmrobin518@gmail.com>
2021-09-11 18:33:47 +0600

    First Login ui design done

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -32,27 +32,37 @@ class _LoginState extends State<Login> {
             child: Column(
               children: [
                 Padding(
+                    padding: const EdgeInsets.only(left: 20, top: 80, right: 20, bottom: 20),
+                    child: Container(
+                      child: Center(
+                        child: Text(
+                          'Authentication Page',
+                          style: new TextStyle(
+                            fontSize: 30.0,
+                            color: Colors.black87,
+                          ),
+                        ),
+                      ),
+                    )
+                ),
+                Padding(
                   padding: const EdgeInsets.only(
-                      left: 20, top: 10, right: 20, bottom: 0),
+                      left: 20, top: 10, right: 20, bottom: 10),
                   child: Container(
                     child: Image(
                       image: AssetImage('assets/login.jpg'),
                       fit: BoxFit.cover,
                       width: double.infinity,
-                      height: 250,
+                      height: 230,
                     ),
                   ),
-                ),
-                Padding(
-                    padding: const EdgeInsets.only(left: 20, top: 10, right: 20, bottom: 0),
-                    child: Container(
-                    )
                 ),
                 Padding(
                   padding: const EdgeInsets.only(
                       left: 20, top: 10, right: 20, bottom: 0),
                   child: TextField(
                     decoration: InputDecoration(
+                      icon: Icon(Icons.person),
                       labelText: 'User Name',
                       hintText: 'ex: MsM Robin....',
                       enabledBorder: OutlineInputBorder(
@@ -70,7 +80,9 @@ class _LoginState extends State<Login> {
                   padding: const EdgeInsets.only(
                       left: 20, top: 10, right: 20, bottom: 0),
                   child: TextField(
+                    obscureText: true,
                     decoration: InputDecoration(
+                      icon: Icon(Icons.phonelink_lock),
                       labelText: 'Password',
                       enabledBorder: OutlineInputBorder(
                         borderRadius: BorderRadius.all(Radius.circular(5.0)),
@@ -85,12 +97,23 @@ class _LoginState extends State<Login> {
                 ),
                 Padding(
                   padding: const EdgeInsets.only(left: 100, top: 10, right: 100, bottom: 0),
-                  child: RaisedButton(
-                    child: Text('Submit'),
-                    onPressed: (){},
-                    elevation: 10,
-                    focusColor: Colors.blue,
-                    textColor: Colors.blue,
+                  child: ButtonTheme(
+                    minWidth: 200.0,
+                    height: 50.0,
+                    child: RaisedButton(
+                      child: Text(
+                        'Submit',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: (){},
+                      elevation: 10,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(16.0)),
+                      ),
+                    ),
                   ),
                 ),
               ],
