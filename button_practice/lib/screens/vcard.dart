379396a9200baabe3d60206ad974510fca888909
robinNcode <msmrobin518@gmail.com>
2021-09-13 11:52:24 +0600

    card added

--- button_practice/lib/screens/vcard.dart
+++ button_practice/lib/screens/vcard.dart
@@ -25,7 +25,6 @@ class _VcardState extends State<Vcard> {
                 color: Colors.white,
                 fontWeight: FontWeight.bold,
                 fontSize: 40,
-                fontFamily: 'Pacifico'
               ),
             ),
             Text(
@@ -34,15 +33,38 @@ class _VcardState extends State<Vcard> {
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                   fontSize: 20,
-                  fontFamily: 'Pacifico'
-              ),
+                  fontFamily: 'Pacifico'),
             ),
             SizedBox(
+              height: 20,
+              width: 200,
               child: Divider(
                 color: Colors.white,
               ),
             ),
-
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.phone_android),
+                title: Text(
+                  '+8801739299741',
+                  style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                    fontSize: 20
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.mail_outline),
+                title: Text(
+                  'msmrobin518@gmail.com',
+                  style: TextStyle(
+                    color: Colors.lightBlueAccent,
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
