5264f9d57f821d00dfaabec78bf3f79b7580e20b
robinNcode <msmrobin518@gmail.com>
2021-09-13 11:23:18 +0600

    divider added

--- button_practice/lib/screens/vcard.dart
+++ button_practice/lib/screens/vcard.dart
@@ -11,12 +11,38 @@ class _VcardState extends State<Vcard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
         child: Column(
           children: [
             CircleAvatar(
-              backgroundImage: AssetImage('assets/images/instagram.jpg'),
-            )
+              backgroundImage: AssetImage('assets/images/personal.jpg'),
+              radius: 70,
+            ),
+            Text(
+              'MSM Robin',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 40,
+                fontFamily: 'Pacifico'
+              ),
+            ),
+            Text(
+              'Android Developer',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  fontFamily: 'Pacifico'
+              ),
+            ),
+            SizedBox(
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+
           ],
         ),
       ),
